bab6506f67d8058f216733143e1d29564ce5113e
paoloricciuti <ricciutipaolo@gmail.com>
2024-10-10 17:06:30 +0200

    feat: let's try to cache models

--- src/routes/live/swd.ts
+++ src/routes/live/swd.ts
@@ -1,4 +1,13 @@
-import { pipeline, type TextClassificationSingle } from '@xenova/transformers';
+import { pipeline, env, type TextClassificationSingle } from '@xenova/transformers';
+
+// Specify a custom location for models (defaults to '/models/').
+env.localModelPath = '/cached-models/';
+
+// Disable the loading of remote models from the Hugging Face Hub:
+env.allowRemoteModels = false;
+
+// Set location of .wasm files. Defaults to use a CDN.
+env.backends.onnx.wasm.wasmPaths = '/cached-models/';
 
 class PipelineSingleton {
 	static task = 'text-classification' as const;
